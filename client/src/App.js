44f3b6397c8ba060db1bb90bc782a24ac2bee0f0
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-07-31 11:52:38 -0700

    prep for merge with components

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+  
 }
 
 export default App;
