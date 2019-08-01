4e284a509962b9ecf527c5cda2fe61e70791d59f
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-08-01 11:37:10 -0700

    Frontend refactor (#4)
    * add material ui and start on navbar
    
    * fix style for nav
    
    * prep for merge with components
    
    * create components and routes
    
    * comment out some lines temporary for initial work
    
    * google map
    
    * map card
    
    * initialize header, create jumbotron, style
    
    * fix autocomplete drop down styling
    
    * multiple cards
    
    * fix react google map
    
    * Modal
    
    * update styling for header and add logo
    
    * modal component
    
    * edi styling for card shadows

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import NoMatch from "./pages/NoMatch";
+import Home from "./pages/Home";
+// import Favorites from "./pages/Favorites";
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Home} />
+            {/* <Route exact path="/favorites" component={Favorites} /> */}
+            {/* <Route exact path="/noMatch" component={NoMatch} /> */}
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cards from "../../components/Cards"
+
+class Home extends React.Component {
+    render() {
+        return (
+                <Cards />
+        )
+    }
+}
+
+export default Home;
