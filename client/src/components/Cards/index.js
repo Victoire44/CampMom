1604acd80e34018d9cb8634d224fdd60907a8dcb
Victoire <victoirebaron@hotmail.fr>
2019-07-31 21:41:14 -0700

    fix react google map

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -6,16 +6,14 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(
     createStyles({
         card: {
-            // borderRadius: 0,
-            margin: 1
+            // borderRadius: 0
         }
     })
 );
@@ -23,43 +21,49 @@ const useStyles = makeStyles(
 function MapCard(props) {
     const classes = useStyles();
     const campgrounds = ["Yosemite", "Lake Tahoe", "Lake Tahoe", "Lake Tahoe"]
-    return (
-        <Container maxWidth="lg">
-        <div className={classes.root}>
-            <GridList cellHeight={340} spacing={50} className={classes.gridList} cols={3}>
-                {campgrounds.map(result => (
-                    <GridListTile key={result} padding={40} cols={1}>
-                        <Card className={classes.card}>
-                            <div style={{ height: 200 }}>
-                                <Map style={{ height: 200 }} google={props.google} zoom={10}>
-                                    <Marker onClick={props.onMarkerClick}
-                                        name={'Current location'} />
-                                </Map>
 
-                            </div>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Title
+    const Map = withGoogleMap(props => (
+        <GoogleMap
+            defaultCenter={{ lat: 39.833333, lng: -98.583333 }}
+            defaultZoom={3}
+        >
+            <Marker position={{ lat: 39.833333, lng: -98.583333 }} />
+        </GoogleMap>
+    ));
+
+    return (
+        <Container maxWidth="md">
+            <div className={classes.root}>
+                <Grid container justify="center" spacing={7}>
+                    {campgrounds.map(result => (
+                        <Grid key={result} item xs={12} md={4}>
+                            <Card className={classes.card}>
+                                <div style={{ height: 200 }}>
+                                    <Map
+                                        containerElement={<div style={{ height: `100%`, width: `100%` }} />}
+                                        mapElement={<div style={{ height: `100%` }} />}
+                                    />
+                                </div>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Title
                     </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        Description
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Description
                     </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">Favorite</Button>
-                                <Button size="small" color="primary">Learn More</Button>
-                            </CardActions>
-                        </Card>
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
         </Container>
     );
 }
 
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyBmSLZewBiOZn3GJAasNRzdzRYA_Bi4qyo")
-})(MapCard)
+export default MapCard
