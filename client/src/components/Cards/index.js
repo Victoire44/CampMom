d28bf05445a939f24ebe6d8ee7d1a825a5b37dee
Victoire <victoirebaron@hotmail.fr>
2019-08-01 11:11:45 -0700

    modal component

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,30 +1,15 @@
-import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
-import Modal from '@material-ui/core/Modal';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-        margin: theme.spacing(30, 0)
-    },
-    paper: {
-        outline: 'none'
-    }
-}));
+import MyModal from "../Modal"
 
 function MapCard(props) {
-    const classes = useStyles();
-
     const campgrounds = ["Yosemite", "Lake Tahoe", "Lake Tahoe", "Lake Tahoe"]
 
     const Map = withGoogleMap(props => (
@@ -35,16 +20,6 @@ function MapCard(props) {
             <Marker position={{ lat: 39.833333, lng: -98.583333 }} />
         </GoogleMap>
     ));
-
-    const [open, setOpen] = React.useState(false);
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     return (
         <Container maxWidth="md">
@@ -69,24 +44,7 @@ function MapCard(props) {
                                 </CardContent>
                             </CardActionArea>
                             <CardActions>
-                                <Button onClick={handleOpen} size="small" color="primary">Learn More</Button>
-                                <Modal
-                                    aria-labelledby="simple-modal-title"
-                                    aria-describedby="simple-modal-description"
-                                    open={open}
-                                    onClose={handleClose}
-                                >
-                                    <Container maxWidth="md">
-                                        <Paper className={classes.root}>
-                                            <Typography variant="h5" component="h3">
-                                                This is a sheet of paper.
-                                            </Typography>
-                                            <Typography component="p">
-                                                Paper can be used to build surface or other elements for your application.
-                                            </Typography>
-                                        </Paper>
-                                    </Container>
-                                </Modal>
+                                <MyModal />
                             </CardActions>
                         </Card>
                     </Grid>
