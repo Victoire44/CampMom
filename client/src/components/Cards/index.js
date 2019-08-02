027391ce6d792a9e985b6c0f5377937e426a7550
Victoire <victoirebaron@hotmail.fr>
2019-08-01 22:32:13 -0700

    display correct position on map

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -9,46 +9,53 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import MyModal from "../Modal"
 
+
 function MapCard(props) {
     const Map = withGoogleMap(props => (
         <GoogleMap
-            defaultCenter={{ lat: 39.833333, lng: -98.583333 }}
-            defaultZoom={3}
+            defaultCenter={props.position != null ? props.position : { lat: 39.833333, lng: -98.583333 }}
+            defaultZoom={props.position != null ? 11 : 3}
         >
-            <Marker position={{ lat: 39.833333, lng: -98.583333 }} />
+            {props.position != null ? <Marker position={props.position} /> : null}
         </GoogleMap>
     ));
-        
+
     return (
         <Container maxWidth="md" style={{ marginTop: 70 }}>
             <Grid container justify="center" spacing={7}>
-                {console.log(props.campgrounds)}
-                {props.campgrounds.map(campground => (
-                    <Grid key={campground.name} item xs={12} md={4}>
-                        {/* {console.log(campgrounds)} */}
-                        <Card>
-                            <div style={{ height: 200 }}>
-                                <Map
-                                    containerElement={<div style={{ height: `100%`, width: `100%` }} />}
-                                    mapElement={<div style={{ height: `100%` }} />}
-                                />
-                            </div>
-                            <CardActionArea>
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {campground.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        {/* {campground.description} */}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <MyModal />
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                ))}
+                {props.campgrounds.map(campground => {
+                    var latLongString = campground.latLong;
+                    var regex = /[\d\.-]+/g;
+                    var latLong = latLongString.match(regex);
+                    var position = latLong != null ? { lat: parseFloat(latLong[0]), lng: parseFloat(latLong[1]) } : null
+
+                    return (
+                        <Grid key={campground.id} item xs={12} md={4}>
+                            <Card style={{ height: "100%" }}>
+                                <div style={{ height: 200 }}>
+                                    <Map
+                                        position={position}
+                                        containerElement={<div style={{ height: `100%`, width: `100%` }} />}
+                                        mapElement={<div style={{ height: `100%` }} />}
+                                    />
+                                </div>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            {campground.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <MyModal />
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    )
+                })}
             </Grid>
         </Container>
     );
