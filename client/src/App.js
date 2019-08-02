4346c1748ea99b252e8a3238f0ddfac39e744e02
Victoire <38075311+Victoire44@users.noreply.github.com>
2019-08-02 11:57:36 -0700

    Card information (#5)
    * handleCampgrounds
    
    * update card

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,21 @@ import Home from "./pages/Home";
 
 class App extends Component {
 
+  state = {
+      campgrounds: [],
+  }
+
+  handleCampgrounds = campgrounds =>
+    this.setState({ campgrounds: campgrounds })
+
   render() {
     return (
       <Router>
         <div>
           <Navbar />
-          <Header />
+          <Header handleCampgrounds={this.handleCampgrounds} />
           <Wrapper>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" render={props => <Home {...props} campgrounds={this.state.campgrounds} />} />
             {/* <Route exact path="/favorites" component={Favorites} /> */}
             {/* <Route exact path="/noMatch" component={NoMatch} /> */}
           </Wrapper>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+export default {
+    // National park Service data
+    getCampground: function (query) {
+        return axios.get(`https://developer.nps.gov/api/v1/campgrounds?q=${query}&api_key=O4VdhmolNStlPLj2bo2DfPKWks3F8J9xfihpGqTf`);
+    },
+    // OpenWeather data
+    getWeather: function(lat, lon, cnt){
+        return axios.get(`api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}`)
+    },
+}
+
