562386634d0b7d725469eaa36d6ccf85dd2657c9
Derek Goldstone <derekgoldstone@yahoo.com>
2019-08-03 16:48:34 -0700

    Defined user model and added index.js for exporting

---
+++ db/models/Users.js
@@ -0,0 +1,40 @@
+module.exports= function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        name: {
+            type: DataTypes.STRING
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        uuid: {
+            type: DataTypes.UUID,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        trips: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        invitedTo: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        favs: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+    User.associate = function(models) {
+
+        User.hasMany(models.Trips, {
+        });
+
+    };
+
+    return User;
+}
