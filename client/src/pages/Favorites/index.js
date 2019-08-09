7e3d0859a925519d85c3dcb4428db689cb3145d1
Victoire <victoirebaron@hotmail.fr>
2019-08-08 19:44:31 -0700

    circle loading for favorites

--- client/src/pages/Favorites/index.js
+++ client/src/pages/Favorites/index.js
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { setState } from "react";
 import API from "../../utils/API";
 import Cards from "../../components/Cards"
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class Favorites extends React.Component {
     state = {
-        campgrounds: []
+        campgrounds: [],
+        loading: false
     }
 
     componentDidMount = () => {
-        API.getFavoriteCampgrounds().then(campgrounds => {
-            console.log(campgrounds)
-            this.setState({ campgrounds: campgrounds })
-        })
+        this.setState({ loading: true })
+        API.getFavoriteCampgrounds()
+            .then(campgrounds => {
+                this.setState({ campgrounds: campgrounds, loading: false })
+            })
+            .catch(err => {
+                this.setState({ loading: false })
+                console.log(err)
+            })
     }
 
     render() {
         return (
             <div>
-            <h1 style={{ textAlign: "center", fontFamily: "Red Hat Display, sans-serif" }}>My favorites</h1>
-            <Container maxWidth="md" style={{ marginTop: 70 }}>
-                <Cards campgrounds={this.state.campgrounds} />
-            </Container>
+                <h1 style={{ textAlign: "center", fontFamily: "Red Hat Display, sans-serif" }}>My favorites</h1>
+              
+                    <Container maxWidth="md" style={{ marginTop: 70 }}>
+                    {this.state.loading ?
+                    <div style={{ textAlign: "center", color: "#c63d1e" }}>
+                    <CircularProgress color="secondary" /></div> :
+                        <Cards campgrounds={this.state.campgrounds} />
+                    
+                        }
+                    
+                    </Container>
             </div>
         )
     }
